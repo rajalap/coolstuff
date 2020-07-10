b6ed02421190fd1db9e6cd158e088b969e8a1f3e
kwaiidan <philip.rajala@gmail.com>
2020-07-10 16:52:11 -0400

    initial commit of loop detect with makeLL working

--- loopDetect.py
+++ loopDetect.py
@@ -1,5 +1,8 @@
+import makeLL
+
 def main():
     print("Loop Detection")
+    makeLL.main()
 
 if __name__ == "__main__":
     main()
--- makeLL.py
+++ makeLL.py
@@ -1,10 +1,32 @@
-import node
+from node import Node
 
-def make(LL):
-    i = 0
-    head = node.Node([i])
-    i += 1
-    # Should continue until the list is at the end
-    while LL is not None:
-        head
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def AtEnd(self, newdata):
+        NewNode = Node(newdata)
+        if self.head is None:
+            self.head = NewNode
+            return
+        last = self.head
+        while(last.next):
+            last = last.next
+        last.next=NewNode
+    
+    def printList(self):
+        printval = self.head
+        while printval is not None:
+            print(printval.data)
+            printval = printval.next
+
+def main(llSource=["Mon","Tue","Wed"]):
+    print("Make LL")
+    list = SLinkedList()
+    for i, datum in enumerate(llSource):
+        list.AtEnd(llSource[i])
+    list.printList()
+
+if __name__=="__main__":
+    main()    
 
